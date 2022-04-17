2cff722ef7b2df9151e98251663a8165ee00e6f9
Tobias Brummer <tobiasbrummer@lycaknight.de>
2022-04-17 21:09:24 +0000

    Change Script

--- kclean.sh
+++ kclean.sh
@@ -2,34 +2,13 @@
 : '
 ______________________________________________
 
-							Kernel Cleaner
-						  	Lyca Knight
+				Kernel Cleaner
+			     Lyca Knight
 ______________________________________________
-								Original by
-            	by Jordan Hillis
+			 	 Original by
+               by Jordan Hillis
            contact@jordanhillis.com
            https://jordanhillis.com
-______________________________________________
-
-MIT License
-
-Copyright (c) 2018 Jordan S. Hillis
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
 ______________________________________________
 '
 
@@ -66,7 +45,7 @@ function version(){
   exit 1
 }
 
-# Header for PVE Kernel Cleaner
+# Header for Kernel Cleaner
 function header_info {
 echo -e "
 █ █ █▀▀ █▀▀█ █▀▀▄ █▀▀ █   
